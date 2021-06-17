f3422b27051dcbb091d4d4e923f15fd211479854
PRIYANSHU SINGH <47605473+PRIYANSHH@users.noreply.github.com>
2021-06-17 14:48:52 +0530

    Final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 
 const internal=require(__dirname+"/internal.js");
 const app=express();
@@ -11,6 +12,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
 
 app.get("/",function(req,res){
 let day=internal.getDate();
